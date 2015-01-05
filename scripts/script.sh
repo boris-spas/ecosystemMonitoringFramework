27eef78a5f80dfa91f69cf12c797e6f1c46828cf
Boris Spasojevic <boris.spas@yahoo.com>
2015-01-05 13:23:09 +0100

    added iterate_over_projects_parallel

--- scripts/script.sh
+++ scripts/script.sh
@@ -26,8 +26,27 @@ function iterate_over_projects() {
 	done
 }
 
+function iterate_over_projects_parallel() {
+	N=$1 #number of projects
+	ST=`xmllint --xpath "/config/target/analysis/st/text()" $CONFIG `
+
+	# clone simple parallel bash
+	git clone https://github.com/boris-spas/simpleParallelBash.git
+
+	#build the script that runs in parallel
+	echo `pwd`"/pharo `pwd`/$IMAGE eval \"EMFUtils loadProjectWithIndex: \$1. $ST\"" > simpleParallelBash/processOneProject.sh
+
+	#build the input file for simple parallel bash
+	for (( i = 1; i <= $N; i++ )); do
+		echo $i >> simpleParallelBash/input.txt
+	done
+
+	cd simpleParallelBash
+	sh run.sh processOneProject.sh input.txt 4
+	cd ..
+}
 
 function script_main() {
-	iterate_over_projects `count_the_projects`
+	iterate_over_projects_parallel `count_the_projects`
 }
 
